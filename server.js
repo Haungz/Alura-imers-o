5f8f02cd25eb4fd2329521fec334f3f1c5bde7b8
Haungz <news.word.game@gmail.com>
2024-11-19 11:57:47 -0300

    Adiciona uma array com objetos (posts) e adiciona busca por id específico pelo link

--- server.js
+++ server.js
@@ -1,10 +1,51 @@
 import express from "express"
 
+const posts = [
+    {
+        id: 1,
+        descricao: "Uma foto teste",
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id: 2,
+        descricao: "Um gato preguiçoso",
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id: 3,
+        descricao: "Paisagem montanhosa",
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id: 4,
+        descricao: "Cachorro brincando",
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id: 5,
+        descricao: "Comida deliciosa",
+        imagem: "https://placecats.com/millie/300/150"
+    }
+];
+
 const app = express()
+app.use(express.json())
+
 app.listen(3000, () => {
 	console.log("Servidor escutando...")
 })
 
-app.get("/api", (req,res) => {
-	res.status(200).send("Boas vindas à imersão!")
+app.get("/posts", (req,res) => {
+	res.status(200).json(posts)
+})
+
+function buscarPostPorID(id) {
+    return posts.findIndex((post) => {
+        return post.id === Number(id)
+    })
+}
+
+app.get("/posts/:id", (req,res) => {
+	const index = buscarPostPorID(req.params.id)
+    res.status(200).json(posts[index])
 })
