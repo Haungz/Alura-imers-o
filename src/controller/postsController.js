239b035805a662c389cdfb2994d456bff146e017
Haungz <news.word.game@gmail.com>
2024-11-21 19:03:49 -0300

    Adiciona função que posta o post no banco de dados

--- src/controller/postsController.js
+++ src/controller/postsController.js
@@ -1,8 +1,19 @@
 import getTodosPosts from "../models/postModel.js"
 
 
-export async function listarPosts(req,res) {          // Rota para buscar todos os posts//await precisa do async
-	const posts = await getTodosPosts()         // chama a função para buscar todos os posts
-	res.status(200).json(posts)                 // Envia uma resposta http com status 200 (ok) e os posts no formato JSON na porta 2000
+export async function listarPosts(req,res) {              // Rota para buscar todos os posts                 //await precisa do async
+	const posts = await getTodosPosts()                  // chama a função para buscar todos os posts
+	res.status(200).json(posts)                         // Envia uma resposta http com status 200 (ok) e os posts no formato JSON na porta 2000
 } 
 	
+export async function postarNovoPost(req,res) {
+	const novoPost = req.body
+	try {
+		const postCriado = await criarPost(novoPost)
+		res.status(200).json(postCriado)
+	} catch(erro) {
+		console.error(erro.message)
+			res.status(500).json({"Erro": "Falha na requisição"})
+		
+	}
+}
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { listarPosts } from "../controller/postsController.js"
+import { listarPosts, postarNovoPost } from "../controller/postsController.js"
 
 const routes = (app) => {
 	app.use(express.json())                         // Permite que o servidor interprete requisições com corpo no formato JSON
 	app.get("/posts", listarPosts)
+	app.post("/posts", postarNovoPost)
 }
 
 export default routes
