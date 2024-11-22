9771c5e4cebdffe0071b4066984872b4037327b5
Haungz <news.word.game@gmail.com>
2024-11-21 23:23:16 -0300

    Postscontroller faltava uma chave na linha 7 e tava dando erro. corrigido.

--- src/controller/postsController.js
+++ src/controller/postsController.js
@@ -3,20 +3,17 @@ import fs from "fs"
 
 export async function listarPosts(req,res) {              // Rota para buscar todos os posts                 //await precisa do async
 	const posts = await getTodosPosts()                  // chama a função para buscar todos os posts
-	res.status(200).json(posts)                         // Envia uma resposta http com status 200 (ok) e os posts no formato JSON na porta 2000
-export async function listarPosts(req,res) {              // Rota para buscar todos os posts                 //await precisa do async
-	const posts = await getTodosPosts()                  // chama a função para buscar todos os posts
-	res.status(200).json(posts)                         // Envia uma resposta http com status 200 (ok) e os posts no formato JSON na porta 2000
-} 
+	res.status(200).json(posts)                          // Envia uma resposta http com status 200 (ok) e os posts no formato JSON na porta 2000
+  }                             
 	
-export async function postarNovoPost(req,res) {
+  export async function postarNovoPost(req,res) {
 	const novoPost = req.body
 	try {                                                             //o comando se resume a: tenta criar um novo post, 
 		const postCriado = await criarPost(novoPost)             
 		res.status(200).json(postCriado)
 	} catch(erro) {                                                   // se falhar, aparece erro em vez de crashar o servidor
 		console.error(erro.message)
-			res.status(500).json({"Erro": "Falha na requisição"})
+			res.status(500).json({"Erro":"Falha na requisição"})
 		
 	}
 }
@@ -27,6 +24,7 @@ export async function uploadImagem(req,res) {
 		imgUrl: req.file.originalname,
 		alt: ""
 	}
+
 	try {                                                             //o comando se resume a: tenta criar um novo post, 
 		const postCriado = await criarPost(novoPost)    
 		const imgAtualizada = `uploads/${postCriado.insertedId}.png`
@@ -35,6 +33,5 @@ export async function uploadImagem(req,res) {
 	} catch(erro) {                                                   // se falhar, aparece erro em vez de crashar o servidor
 		console.error(erro.message)
 			res.status(500).json({"Erro": "Falha na requisição"})
-		
 	}
 }
