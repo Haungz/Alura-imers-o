0864e20559bff3f61ca39c356a9ef5d35bb10ce5
Haungz <news.word.game@gmail.com>
2024-11-21 21:00:50 -0300

    Adicionado multer, função de upload de imagens e configuração de armazenamento do multer para imagens do windows

--- src/controller/postsController.js
+++ src/controller/postsController.js
@@ -1,5 +1,5 @@
 import {getTodosPosts, criarPost} from "../models/postModel.js"
-
+import fs from "fs"
 
 export async function listarPosts(req,res) {              // Rota para buscar todos os posts                 //await precisa do async
 	const posts = await getTodosPosts()                  // chama a função para buscar todos os posts
@@ -10,6 +10,24 @@ export async function postarNovoPost(req,res) {
 	const novoPost = req.body
 	try {                                                             //o comando se resume a: tenta criar um novo post, 
 		const postCriado = await criarPost(novoPost)             
+		res.status(200).json(postCriado)
+	} catch(erro) {                                                   // se falhar, aparece erro em vez de crashar o servidor
+		console.error(erro.message)
+			res.status(500).json({"Erro": "Falha na requisição"})
+		
+	}
+}
+
+export async function uploadImagem(req,res) {
+	const novoPost = {
+		descricao: "", 
+		imgUrl: req.file.originalname,
+		alt: ""
+	}
+	try {                                                             //o comando se resume a: tenta criar um novo post, 
+		const postCriado = await criarPost(novoPost)    
+		const imgAtualizada = `uploads/${postCriado.insertedId}.png`
+		fs.renameSync(req.file.path, imgAtualizada)         
 		res.status(200).json(postCriado)
 	} catch(erro) {                                                   // se falhar, aparece erro em vez de crashar o servidor
 		console.error(erro.message)
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,10 +1,23 @@
 import express from "express"
-import { listarPosts, postarNovoPost } from "../controller/postsController.js"
+import { listarPosts, postarNovoPost, uploadImagem } from "../controller/postsController.js"
+import multer from "multer"
 
-const routes = (app) => {
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+})
+
+const upload = multer({ dest: "./uploads" , storage})
+
+const routes = (app) => {                           // Define as rotas usando o objeto express "app"
 	app.use(express.json())                         // Permite que o servidor interprete requisições com corpo no formato JSON
 	app.get("/posts", listarPosts)                  // Rota para buscar todos os posts
 	app.post("/posts", postarNovoPost)              // Rota para criar um post
+	app.post("/upload", upload.single("imagem"), uploadImagem)      // rota para upload de imagens
 }
 
 export default routes
