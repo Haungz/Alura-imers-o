998600a9f54a6e6c8da064e042d1efb5c3fc4f28
Haungz <news.word.game@gmail.com>
2024-11-22 14:59:30 -0300

    Descrição da imagem agora gerada por IA

--- src/controller/postsController.js
+++ src/controller/postsController.js
@@ -1,5 +1,6 @@
 import {getTodosPosts, criarPost, atualizarPost} from "../models/postModel.js"
 import fs from "fs"
+import gerarDescricaoComGemini from "../services/geminiService.js"
 
 export async function listarPosts(req,res) {              // Rota para buscar todos os posts                 //await precisa do async
 	const posts = await getTodosPosts()                  // chama a função para buscar todos os posts
@@ -39,13 +40,17 @@ export async function uploadImagem(req,res) {
 export async function atualizarNovoPost(req,res) {
 	const id = req.params.id
 	const urlImagem = `http://localhost:3000/${id}.png`
-	const post = {
-		imgUrl: urlImagem,
-		descricao: req.body.descricao,
-		alt: req.body.alt
-	}
-	try {                                                             //o comando se resume a: tenta criar um novo post, 
-		const postCriado = await atualizarPost(id,post)             
+	try {                                                                //o comando se resume a: tenta criar um novo post, 
+		const imgBuffer = fs.readFileSync(`uploads/${id}.png`)
+		const descricao = await gerarDescricaoComGemini(imgBuffer)
+
+		const post = {
+			imgUrl: urlImagem,
+			descricao: descricao,
+			alt: req.body.alt
+		}                                       
+
+		const postCriado = await atualizarPost(id,post)    
 		res.status(200).json(postCriado)
 	} catch(erro) {                                                   // se falhar, aparece erro em vez de crashar o servidor
 		console.error(erro.message)
